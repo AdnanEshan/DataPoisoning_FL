85a664b5358786d1ad2691062ff8a21a8b26ecd1
AdnanEshan <125243403+AdnanEshan@users.noreply.github.com>
2023-04-27 01:27:26 +0600

    Update attack_timing.py

--- attack_timing.py
+++ attack_timing.py
@@ -4,15 +4,26 @@ from federated_learning.utils import replace_1_with_9
 from federated_learning.utils import replace_4_with_6
 from federated_learning.utils import replace_1_with_3
 from federated_learning.utils import replace_6_with_0
+from federated_learning.worker_selection import PoisonerProbability
+import numpy as np
 from federated_learning.worker_selection import BeforeBreakpoint
 from federated_learning.worker_selection import AfterBreakpoint
 from server import run_exp
 
+def replace_1_with_9_30percent(data):
+    new_data = []
+    for d in data:
+        if d == 1 and np.random.rand() < 0.3:
+            new_data.append(9)
+        else:
+            new_data.append(d)
+    return new_data
+
 if __name__ == '__main__':
     START_EXP_IDX = 3000
-    NUM_EXP = 1
-    NUM_POISONED_WORKERS = 25
-    REPLACEMENT_METHOD = replace_1_with_9
+    NUM_EXP = 3
+    NUM_POISONED_WORKERS = 0
+    REPLACEMENT_METHOD = replace_1_with_9_30percent
     KWARGS = {
         "BeforeBreakPoint_EPOCH" : 75,
         "BeforeBreakpoint_NUM_WORKERS_PER_ROUND" : 5,
